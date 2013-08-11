818f8a55fd9e726125bb68edc40f0eff1ee51c04
Ahmad Shiina <ahmad.n.shiina@gmail.com>
2013-08-12 00:38:59 +0900

    Added batchPutItem() method; Added Limit & options field on query()

--- dynamodb_model.php
+++ dynamodb_model.php
@@ -16,6 +16,14 @@ class Dynamodb_model extends CI_Model {
 	protected $enumTypeClass = 'Aws\DynamoDb\Enum\Type';
 	protected $enumCOClass = 'Aws\DynamoDb\Enum\ComparisonOperator';
 
+	/*
+	 * batch execution limit
+	 */
+	protected $BATCH_LIMIT = 25;
+
+	/*
+	 * Constructor
+	 */
 	public function __construct() {
 		$this->load->library('awslib');
 		$this->ddbClient = $this->awslib->aws->get('dynamodb');
@@ -28,9 +36,13 @@ class Dynamodb_model extends CI_Model {
 	 * The conditions' keys will take a operator of the following:
 	 * < , <= , > , >= , = , != , ~(BETWEEN operator)
 	 *
+	 * Optionally pass any other query API options in $options.
+	 * 
 	 * @param string tableName
-	 * @param array conditions
-	 * return array items
+	 * @param array  conditions
+	 * @param int    limit
+	 * @param array  options
+	 * return array  items
 	 *
 	 * example $conditions format:
 array(
@@ -38,15 +50,18 @@ array(
         'Date ~'   => array ( (float)1376030000, (float)1376040000)
 ));
 	 */
-	public function query ($tableName, $conditions) {
+	public function query ($tableName, $conditions, $limit=10, $options=array()) {
 		try {
-			$qResult = $this->ddbClient->query(array(
+			$qRequest = array(
 				'TableName'		=> $tableName,
 				'HashKeyValue'	=> array(
 					$this->_getEnumType($primaryKey) => $primaryKey,
 				),
 				'KeyConditions'	=> $this->_formatQueryCondition($conditions)
-			));
+			);
+			$qRequest['Limit'] = $limit;
+			$qRequest = array_merge($qRequest, $options);
+			$qResult = $this->ddbClient->query($qRequest);
 		} catch (Exception $e) {
 			error_log($e->getMessage());
 		}
@@ -69,7 +84,7 @@ array(
         'Id'       => (int)251,
         'Comment'  => (string)"hello",
         'Date'     => (float)1376038837
-));
+);
 	 */
 	public function putItem ($tableName, $item) {
 		try {
@@ -77,6 +92,72 @@ array(
 			$this->ddbClient->putItem(array(
 				'TableName'		=> $tableName,
 				'Item'			=> $formattedItem
+			));
+			return true;
+		} catch (Exception $e) {
+			error_log($e->getMessage());
+			return false;
+		}
+	}
+
+	/*
+	 * batchWriteItem wrapper for dynamodb, only for putItem requests.
+	 * It will take an array of assoc-arrays for items to insert, 
+	 * and turn it into a DynamoDB API compliant format, then send a request.
+	 * Make sure to cast each value, since the program will dynamically
+	 * set the appropriate Enum based on the variable type.
+	 * 
+	 * The DynamoDB API only takes max 25items per batch, 
+	 * so larger batches will be automatically divided into multiple requests.
+	 *
+	 * @param string tableName
+	 * @param array items
+	 * return void
+	 *
+	 * example $items format:
+array(
+    array(
+        'Id'       => (int)251,
+        'Comment'  => (string)"hello",
+        'Date'     => (float)1376038837
+    ),
+    array(
+        'Id'       => (int)251,
+        'Comment'  => (string)"goodbye",
+        'Date'     => (float)1376038880
+    )
+);
+	 */
+	public function batchPutItem ($tableName, $items) {
+		$formattedItems = array();
+		$i=1;
+		foreach ($items as $item) {
+			$formattedItems[] = array(
+				"PutRequest" => array(
+					"Item" => $this->_formatPutItem($item)
+				)
+			);
+
+			// execute on each batch limit
+			if (count($formattedItems) >= $this->BATCH_LIMIT) {
+				error_log("execute batch write: $i");
+				$this->_executeBatchWriteItem($tableName, $formattedItems);
+				unset($formattedItems);
+				$formattedItems = array();
+			}
+			$i++;
+		}
+		$this->_executeBatchWriteItem($tableName, $formattedItems);
+		unset($items);
+		unset($formattedItems);
+	}
+
+	protected function _executeBatchWriteItem ($tableName, $formattedItems) {
+		try {
+			$this->ddbClient->batchWriteItem(array(
+				'RequestItems'	=> array(
+					$tableName => $formattedItems
+				)
 			));
 		} catch (Exception $e) {
 			error_log($e->getMessage());
@@ -86,7 +167,7 @@ array(
 	/*
 	 * (protected) format item into dynamodb style.
 	 * example result format:
-    'Item'      => array(
+    array(
         'id'    => array('N' => '3000'),
         'time'  => array('N' => $time),
         'error' => array('S' => 'Out of bounds'),
